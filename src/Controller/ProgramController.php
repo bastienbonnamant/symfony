a6081ffa98b99f22567960b9ef582cbce2c7030c
bastienbonnamant <bastien.bonnamant@gmail.com>
2021-05-21 13:01:39 +0200

    quete symfony 5

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -8,13 +8,25 @@ use Symfony\Component\Routing\Annotation\Route;
 
 Class ProgramController extends AbstractController
 {
-   /**
- * @Route("/programs/", name="program_index")
- */
-public function index(): Response
-{
-    return $this->render('program/index.html.twig', [
-       'website' => 'Wild Séries',
-    ]);
-}
+    /**
+     * @Route("/programs/", name="program_index")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('program/index.html.twig', [
+            'website' => 'Wild Séries',
+        ]);
+    }
+    /** 
+     * @param int $id
+     * @return Response
+    * @Route("/programs/{id}", requirements={"id"="\d+"}, methods={"GET"}, name="show")
+    */
+    public function show(int $id) : Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id
+            ]);
+    }
 }
