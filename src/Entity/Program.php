66b99bab6255a1a77a644aff22c79b09dc3d5c8f
bastienbonnamant <bastien.bonnamant@gmail.com>
2021-05-25 10:56:44 +0200

    quets symfony 7

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -37,6 +37,12 @@ class Program
      */
     private $poster;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $category;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -86,6 +92,18 @@ class Program
     public function setPoster(string $poster): self
     {
         $this->poster = $poster;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
